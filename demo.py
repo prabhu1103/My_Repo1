fb28bbff9bbda0aca318d6a058096cf978df5974
prabhu1103 <m.prabhu0395@gmail.com>
2025-08-03 12:30:47 +0530

    My first py file

---
+++ demo.py
@@ -0,0 +1,22 @@
+# print("Hello World!!!")
+# a=100
+# b=2
+# print (a+b)
+# print (a-b)
+# print (a*b)
+# print (a/b)
+
+# first_name = "Prabhu"
+# last_name = "M"
+# age = 29
+# is_student = False
+# height = 5.9
+# is_working = True
+
+# print (first_name, last_name, age, is_working)
+
+text = "Handle"
+print (text + "is a red")
+print (text *3)
+print (text.lower())
+print (text.upper())
